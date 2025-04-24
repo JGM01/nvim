7813067f33f4acaab1e094ddb5a0722e3ac68c3c
JGM01 <jacobgermanamccray@gmail.com>
2025-04-23 23:36:58 -0400

    some quick changes

--- lua/jgm/remap.lua
+++ lua/jgm/remap.lua
@@ -7,16 +7,17 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.opt.clipboard = 'unnamedplus'
 
 -- Copy to system clipboard
-vim.keymap.set('n', '<leader>y', '"+y', { desc = 'Copy to system clipboard' })
-vim.keymap.set('v', '<leader>y', '"+y', { desc = 'Copy to system clipboard' })
+vim.keymap.set('n', '<leader>y', '"+y', { desc = 'Copy to system clipboard NORMAL' })
+vim.keymap.set('v', '<leader>y', '"+y', { desc = 'Copy to system clipboard VISUAL' })
 vim.keymap.set('n', '<leader>Y', '"+Y', { desc = 'Copy line to system clipboard' })
 
 -- Paste from system clipboard
-vim.keymap.set('n', '<leader>p', '"+p', { desc = 'Paste from system clipboard' })
-vim.keymap.set('v', '<leader>p', '"+p', { desc = 'Paste from system clipboard' })
+vim.keymap.set('n', '<leader>p', '"+p', { desc = 'Paste from system clipboard NORMAL' })
+vim.keymap.set('v', '<leader>p', '"+p', { desc = 'Paste from system clipboard VISUAL' })
 vim.keymap.set('n', '<leader>P', '"+P', { desc = 'Paste line from system clipboard' })
 
 -- Select all text in file
 vim.keymap.set('n', '<leader>a', 'ggVG', { desc = 'Select all text' })
 -- Optional: Select all and copy to system clipboard
-vim.keymap.set('n', '<leader>A', 'ggVG"+y', { desc = 'Select all and copy to clipboard' })
+vim.keymap.set('n', '<leader>A', 'ggVG"+y', { desc = 'Select all and copy to clipboard NORMAL' })
+vim.keymap.set('v', '<leader>A', 'ggVG"+y', { desc = 'Select all and copy to clipboard VISUAL' })
