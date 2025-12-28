2951fd26df411bd83250ab7a606ae730b5a2bf29
JGM01 <jacobgermanamccray@gmail.com>
2025-12-27 22:36:51 -0500

    test

--- init.lua
+++ init.lua
@@ -31,7 +31,19 @@ vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { noremap = true, silent = true, desc = 'Rename symbol' })
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true, desc = 'Go to definition' })
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, { noremap = true, silent = true, desc = 'Go to references' })
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { noremap = true, silent = true, desc = 'Go to implementation' })
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { noremap = true, silent = true, desc = 'Code action' })
+
+vim.keymap.set('n', 'gD', function()
+	require('telescope.builtin').lsp_definitions()
+end, { desc = 'Peek definition(s)' })
+
+vim.keymap.set('n', 'gI', function()
+	require('telescope.builtin').lsp_implementations()
+end, { desc = 'Peek implementations' })
+
+vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help,
+	{ noremap = true, silent = true, desc = 'Signature help' })
 
 -- Plugins
 vim.pack.add({
@@ -57,7 +69,7 @@ vim.pack.add({
 
 -- Treesitter config
 require('nvim-treesitter.configs').setup({
-	ensure_installed = { "lua", "c", "cpp", "rust", "python", "json", "html", "css", "javascript", "wgsl" },
+	ensure_installed = { "lua", "c", "cpp", "rust", "python", "json", "html", "css", "javascript", "wgsl", "markdown" },
 	highlight = { enable = true },
 	indent = { enable = true },
 	rainbow = { enable = true, extended_mode = true },
@@ -65,7 +77,8 @@ require('nvim-treesitter.configs').setup({
 
 -- LSP setup with cmp integration
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-vim.lsp.enable({ "lua_ls", "clangd", "rust_analyzer", "wgsl-analyzer" }, { capabilities = capabilities })
+vim.lsp.enable({ "lua_ls", "clangd", "rust_analyzer", "wgsl-analyzer", "protols", "markdown-oxide" },
+	{ capabilities = capabilities })
 
 -- Telescope mappings
 local builtin = require('telescope.builtin')
@@ -78,30 +91,63 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help ta
 local cmp = require('cmp')
 local luasnip = require('luasnip')
 
+
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
 cmp.setup({
+	-- How completion + docs windows look
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+
+	-- Snippet expansion
 	snippet = {
 		expand = function(args)
 			luasnip.lsp_expand(args.body)
 		end,
 	},
+
+	-- Key mappings while in insert mode
 	mapping = cmp.mapping.preset.insert({
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<CR>'] = cmp.mapping.confirm({ select = true }),
+
 		['<Tab>'] = cmp.mapping.select_next_item(),
 		['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+		-- Scroll documentation window
+		['<C-d>'] = cmp.mapping.scroll_docs(4),
+		['<C-u>'] = cmp.mapping.scroll_docs(-4),
+
+		-- Abort completion
+		['<C-e>'] = cmp.mapping.abort(),
 	}),
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		{ name = 'luasnip' },
-	}, {
-		{ name = 'buffer' },
-		{ name = 'path' },
-	}),
+
+	-- Completion sources
+	sources = cmp.config.sources(
+		{
+			{ name = 'nvim_lsp' },
+			{ name = 'luasnip' },
+		},
+		{
+			{ name = 'buffer' },
+			{ name = 'path' },
+		}
+	),
+
+	-- How items are displayed
 	formatting = {
 		format = require('lspkind').cmp_format({
 			mode = 'symbol_text',
-			maxwidth = 50,
-			ellipsis_char = '...',
+			maxwidth = 60,
+			ellipsis_char = '…',
 		}),
+	},
+
+	-- Experimental features (stable enough now)
+	experimental = {
+		ghost_text = true, -- inline suggestion preview
 	},
 })
